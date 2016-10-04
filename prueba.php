77d8d92be31a1abe692e8fba27a25f1feefe4cc6
tomimagnelli <tomas.magnelli@hotmail.com>
2016-10-04 18:50:25 -0300

    prueba

---
+++ prueba.php
@@ -0,0 +1,9 @@
+<?php
+ $db_host="localhost";
+ $db_user="grupo55";
+ $db_pass="ahT0aShiiw";
+ $db_base="grupo55";
+ $cn = new PDO("mysql:dbname=$db_base;host=$db_host",$db_user,$db_pass);
+ $query = "SELECT * FROM categoria”;
+ $result=$cn->query($query);
+?>
