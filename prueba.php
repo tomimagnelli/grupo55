b340581eb6f18a62aa755ea5db5e1e072fca576c
tomimagnelli <tomas.magnelli@hotmail.com>
2016-10-04 20:35:57 -0300

    test

--- prueba.php
+++ prueba.php
@@ -5,6 +5,12 @@
  $db_base="grupo55";
  $cn = new PDO("mysql:dbname=$db_base;host=$db_host",$db_user,$db_pass);
  $query = "SELECT * FROM categoria";
+ $sentencia = $gbd->prepare("INSERT INTO categoria (nombre)
+ VALUES (:name, :value)");
+$sentencia->bindParam(':name', $nombre);
+// insertar una fila
+$nombre = 'cereales';
+$sentencia->execute();
  $result=$cn->query($query)->fetchAll();;
 
 foreach( $result as $row ) {
